29e24f6ec58e6cfc658844c22693ada9660618f0
shelton-benjamin <bshelton906@gmail.com>
2019-08-17 17:04:54 -0400

    Checking work

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,18 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires 5 or more students.");
+            }
+
+
+            return 'F';
+
         }
     }
 }
